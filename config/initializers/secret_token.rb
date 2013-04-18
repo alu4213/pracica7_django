780984f44e10fd63f2de467cd5fdc8293879171b
EliezerCruz <1985eliezer@gmail.com>
2013-04-18 11:09:50 +0100

    Copia practica 6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '71ff423b5a83737471e813dd02d44499ee68ce0c6b02bef2f005213915ed27d47910a3e2607f2eb652467ed23bd95153c3c97102b5f2d880ddb0756768edf3e2'
